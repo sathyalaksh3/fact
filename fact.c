40595270bc44bd3a92d446bdb62795920186151f
sathyalaksh3 <30349849+sathyalaksh3@users.noreply.github.com>
2017-08-15 12:37:59 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -5,9 +5,9 @@ int n=5,fact=1;
 printf("the given number is:%d",n);
 for(int i=1;i<5;i++)
 {
-fact=fact*i;
-printf("%d",fact);
+  fact=fact*i;
 }
+printf("\nthe factorial is:%d",fact);
 return 0;
 }
 
